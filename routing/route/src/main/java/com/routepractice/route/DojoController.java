4692b0652a4a2babc01321428d5d90c425300bf7
Keon Vazir <kvazir@Keons-MacBook-Air.local>
2019-10-05 20:08:24 -0700

    routepractice

---
+++ routing/route/src/main/java/com/routepractice/route/DojoController.java
@@ -0,0 +1,29 @@
+package com.routepractice.route;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@SpringBootApplication
+@RestController
+
+
+public class DojoController{
+	public static void main(String[] args) {
+		SpringApplication.run(RouteApplication.class, args);
+	}
+	@RequestMapping("/{dojo}")
+    public String showLesson(@PathVariable("dojo") String dojo){
+    	return "I love the dojo party!";
+    }
+	@RequestMapping("/{dojo}/{burbank-dojo}")
+	public String showBurbank(@PathVariable("dojo") String dojo, @PathVariable("burbank-dojo") String burbankDojo) {
+		return "I live in Burbank!";
+	}
+	@RequestMapping("/{dojo}/{burbank-dojo}/{san-jose}")
+	public String showJose(@PathVariable("dojo") String dojo, @PathVariable("burbank-dojo") String burbankDojo, @PathVariable("san-jose") String sanJose) {
+		return "I live in San Jose, he lives in Burbank!";
+	}
+}
