94092af15b6e637c4fb0b0e44b7bd95506259c23
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-12-03 20:28:54 -0800

    intial

---
+++ tjek/tjek/src/main/java/com/codingdojo/tjek/TjekApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.tjek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjekApplication.class, args);
+	}
+
+}
---
+++ tjek/tjek/src/test/java/com/codingdojo/tjek/TjekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.tjek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
